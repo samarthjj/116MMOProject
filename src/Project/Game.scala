aeffb73dc35f1c5a90e82f7b7ad1f318604129cd
dwang2770 <dwang2770@bths.edu>
2019-03-03 15:27:49 -0500

    dave's edit

--- src/Project/Game.scala
+++ src/Project/Game.scala
@@ -14,6 +14,7 @@ class Game
     val x: Int = 1 + random.nextInt((99 - 1) + 1)
     val y: Int = 1 + random.nextInt((99 - 1) + 1)
       // this generates a random number between 1 and 99 (both inclusive)
+    //sasfafasf
 
     val player: Player = new Player("name",x ,y)
   }
