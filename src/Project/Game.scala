1f7420d342e92beeece2915989ec643661efd52d
dwang2770 <dwang2770@bths.edu>
2019-03-03 15:27:05 -0500

    dave's edit

--- src/Project/Game.scala
+++ src/Project/Game.scala
@@ -1,7 +1,20 @@
 package Project
 
 import
+import objects.Player
 
-class Game {
+class Game
+{
+  /*
+    since the map is bordered by walls
+   */
+  def spawnPlayer(): Unit =
+  {
+    val random = scala.util.Random
+    val x: Int = 1 + random.nextInt((99 - 1) + 1)
+    val y: Int = 1 + random.nextInt((99 - 1) + 1)
+      // this generates a random number between 1 and 99 (both inclusive)
 
+    val player: Player = new Player("name",x ,y)
+  }
 }
