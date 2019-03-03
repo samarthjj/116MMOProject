c143c2e4d355289d7cfac58dd1315df689daa993
dwang2770 <dwang2770@bths.edu>
2019-03-03 16:03:10 -0500

    dave's edit

--- src/tests/tt.scala
+++ src/tests/tt.scala
@@ -1,6 +1,15 @@
 package tests
 
-class tt
-{
+  object testing
+  {
+    def main(args: Array[String]): Unit =
+    {
+      val random = scala.util.Random
+      for (value <- 1 to 100)
+        {
+          val x: Int = 1 + random.nextInt((99 - 1) + 1)
+          println(x)
+        }
+    }
+  }
 
-}
