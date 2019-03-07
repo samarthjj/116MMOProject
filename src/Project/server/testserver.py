fa7e7e69ddf79e4d61a1ccc8194dcf92cb0b2de2
samarthjuneja <samarthj@buffalo.edu>
2019-03-07 18:31:27 -0500

    Samarth's Edit

--- src/Project/server/testserver.py
+++ src/Project/server/testserver.py
@@ -1,28 +1,25 @@
 import sqlite3
 import json
 
+connect = sqlite3.connect("playerrecord.db")
+cur = connect.cursor()
+
 
 def get_players():
-    connect = sqlite3.connect("playerrecord.db")
-    cur = connect.cursor()
     players = []
-    cur.execute("SELECT * FROM players")
+    cur.execute("SELECT name  FROM players")
     for row in cur:
         players.append(row)
     return json.dumps(players)
 
 
 def add_player(name, x, y, token):
-    connect = sqlite3.connect("playerrecord.db")
-    cur = connect.cursor()
     cur.execute("INSERT INTO players VALUES(?,?,?,?)", (name, x, y, token))
     connect.commit()
     connect.close()
 
 
 def remove_player(name):
-    connect = sqlite3.connect("playerrecord.db")
-    cur = connect.cursor()
     cur.execute("DELETE FROM players WHERE name=?", (name,))
     connect.commit()
     connect.close()
